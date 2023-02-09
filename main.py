c1c6c1b7deefa3548f6634324dbedb06aa211291
Seth Christmus <seth.christmus@fdmgroup.com>
2023-02-09 14:22:37 -0500

    first commit, project version 1 complete - two questions + user input concatenation

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to the super cool, super easy Band Name Generator. Get ready to rock!")
+
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+band_name = city + " " + pet_name
+
+print("Your band name could be " + band_name)
